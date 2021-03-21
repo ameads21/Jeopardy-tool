391c8d629d74f5b760a9ead29f9e70311ce169f0
Aaron Meads <aaron.meads@gmail.com>
2021-03-20 22:01:52 -0600

    Adding loading spinner to login/register

--- Front-End/jeopardy-tool/src/components/Register Login/Register.js
+++ Front-End/jeopardy-tool/src/components/Register Login/Register.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Alert from "../../helpers/Alert";
 import AuthContext from "../../context/AuthContext";
+import LoadingSpinner from "../../helpers/LoadingSpinner";
 
 function Register() {
   const { register } = useContext(AuthContext);
@@ -9,6 +10,7 @@ function Register() {
   const INITIAL_STATE = { username: "", password: "", email: "" };
   const [formData, setFormData] = useState(INITIAL_STATE);
   const [formErrors, setFormErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,7 +22,9 @@ function Register() {
 
   async function handleSubmit(e) {
     e.preventDefault();
+    setIsLoading(true);
     let results = await register(formData);
+    setIsLoading(false);
     if (results.success) {
       history.push(`/${formData["username"]}/projects`);
     } else {
@@ -78,13 +82,16 @@ function Register() {
               {formErrors.length ? (
                 <Alert type="danger" messages={formErrors} />
               ) : null}
-
-              <button
-                className="btn btn-primary float-right"
-                onSubmit={handleSubmit}
-              >
-                Submit
-              </button>
+              {!isLoading ? (
+                <button
+                  className="btn btn-primary float-right"
+                  onSubmit={handleSubmit}
+                >
+                  Submit
+                </button>
+              ) : (
+                <LoadingSpinner />
+              )}
             </form>
           </div>
         </div>
--- Front-End/jeopardy-tool/src/helpers/LoadingSpinner.js
+++ Front-End/jeopardy-tool/src/helpers/LoadingSpinner.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Spinner } from "react-bootstrap";
 
 /** Loading message used by components that fetch API data. */
 
 function LoadingSpinner() {
-  return <div className="LoadingSpinner">Loading...</div>;
+  return (
+    <Spinner animation="border" role="status">
+      <span className="sr-only">Loading...</span>
+    </Spinner>
+  );
 }
 
 export default LoadingSpinner;
